dd879bed4d3f1a4f0248b760ff4ea0303ddf3b41
Gyandeep <gyandeepmehra370@gmail.com>
2021-11-27 08:53:08 +0530

    Add figlet file

---
+++ NPM_Class-4/figlet.js
@@ -0,0 +1,30 @@
+const figlet = require('figlet');
+
+//Basic external module, demo!
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+figlet.text('Boo!', {
+    font: 'Ghost',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+
+//How to install a package!
+//Go to the folder you want to install the package from the terminal
+//and type -> npm install <package_name>
