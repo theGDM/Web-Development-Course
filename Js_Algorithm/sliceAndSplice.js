2519aded06a532f03707de7055149fe16ee9c344
Gyandeep <gyandeepmehra370@gmail.com>
2021-12-04 08:40:35 +0530

    slice and splice

---
+++ Js_Algorithm/sliceAndSplice.js
@@ -0,0 +1,14 @@
+// You are given two arrays and an index.
+// Copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+  let tempArray = [...arr2];
+  tempArray.splice(n,0,...arr1);
+  return tempArray;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5], 1));
+console.log(frankenSplice([1, 2], ["a", "b"], 1));
+console.log(frankenSplice(["claw", "tentacle"], ["head", "shoulders", "knees", "toes"], 2));
