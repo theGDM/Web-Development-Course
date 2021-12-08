174e34eb2eed5d3a0458f53f6bf072ef9da477f3
Gyandeep <gyandeepmehra370@gmail.com>
2021-12-08 09:33:53 +0530

    Add tree file

---
+++ File_System_Organizer_Class-5/commands/tree.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+function treeFn(dirPath) {
+    if (dirPath == undefined) {
+        console.log("Please enter a Directory Path")
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if (doesExist) {
+            treeHelper(dirPath, " ");
+        }
+    }
+}
+
+function treeHelper(targetPath, indent) {
+    let isFile = fs.lstatSync(targetPath).isFile();
+
+    if (isFile) {
+        let fileName = path.basename(targetPath);
+        console.log(indent + "├── " + fileName);
+    } else {
+        let dirName = path.basename(targetPath);
+        console.log(indent + "└── " + dirName);
+
+        let children = fs.readdirSync(targetPath);
+
+        for (let i = 0; i < children.length; ++i){
+            let childPath = path.join(targetPath, children[i]);
+            treeHelper(childPath, indent + '\t');
+        }
+    }
+}
+
+module.exports = {
+    treeFn: treeFn
+}
