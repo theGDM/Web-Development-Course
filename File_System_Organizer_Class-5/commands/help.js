3bcc3176d23df7a1bf46cfe07a924034eedd1d6f
Gyandeep <gyandeepmehra370@gmail.com>
2021-12-08 09:33:04 +0530

    Add help file

---
+++ File_System_Organizer_Class-5/commands/help.js
@@ -0,0 +1,10 @@
+function helpFn() {
+    console.log(`List of all the valid commands -
+    1) Tree Command - node FO.js tree <dirName>
+    2) Organize Command - node FO.js organizer <dirName>
+    3) Help Command - node FO.js help`);
+}
+
+module.exports = {
+    helpFn: helpFn
+}
