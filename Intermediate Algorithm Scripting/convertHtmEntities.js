69829dbceed175c0bf773211dc654ffa3a649304
Gyandeep <gyandeepmehra370@gmail.com>
2022-01-16 09:39:39 +0530

    Add convert Html Entities

---
+++ Intermediate Algorithm Scripting/convertHtmEntities.js
@@ -0,0 +1,27 @@
+function convertHTML(str) {
+    let tempArr = str.split("");
+    for (let i = 0; i < tempArr.length; ++i) {
+        switch (tempArr[i]) {
+            case "&": tempArr[i] = "&amp;";
+                break;
+            case "<": tempArr[i] = "&lt;";
+                break;
+            case ">": tempArr[i] = "&gt;";
+                break;
+            case "'": tempArr[i] = "&apos;";
+                break;
+            case '"': tempArr[i] = "&quot;";
+                break;
+        }
+    }
+    let resStr = tempArr.join("");
+    return resStr;
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+console.log(convertHTML("Sixty > twelve"));
+console.log(convertHTML('Stuff in "quotation marks"'));
+console.log(convertHTML("Schindler's List"));
+console.log(convertHTML("<>"));
+console.log(convertHTML("abc"));
