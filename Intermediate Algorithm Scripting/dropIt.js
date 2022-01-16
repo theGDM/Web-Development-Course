16103d04b626d1944d8a80cc94cf9c33bad8e730
Gyandeep <gyandeepmehra370@gmail.com>
2022-01-16 09:41:28 +0530

    Add drop it

---
+++ Intermediate Algorithm Scripting/dropIt.js
@@ -0,0 +1,26 @@
+// Given the array arr, iterate through and remove each element starting from the first element(the 0 index) until the 
+//function func returns true when the iterated element is passed through it.
+
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+function dropElements(arr, func) {
+    for (let i = 0; i < arr.length; ++i) {
+        let res = func(arr[i]);
+        if (!res) {
+            arr.splice(i, 1);
+            --i;
+        } else {
+            return arr;
+        }
+    }
+    return arr;
+}
+
+console.log(dropElements([1, 2, 3], function (n) { return n < 3; }));
+console.log(dropElements([1, 2, 3, 4], function (n) { return n >= 3; }));
+console.log(dropElements([0, 1, 0, 1], function (n) { return n === 1; }));
+console.log(dropElements([1, 2, 3], function (n) { return n > 0; }));
+console.log(dropElements([1, 2, 3, 4], function (n) { return n > 5; }));
+console.log(dropElements([1, 2, 3, 9, 2], function (n) { return n > 2; }));
+console.log(dropElements([1, 2, 3, 7, 4], function (n) { return n > 3; }));
+console.log(dropElements([1, 2, 3, 9, 2], function (n) { return n > 2; }));
