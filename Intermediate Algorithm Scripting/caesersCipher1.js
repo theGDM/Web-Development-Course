cc9b21969ebb42cbdfec804fd3c27623fce29b8a
Gyandeep <gyandeepmehra370@gmail.com>
2022-01-23 08:43:22 +0530

    Add caesers cipher

---
+++ Intermediate Algorithm Scripting/caesersCipher1.js
@@ -0,0 +1,43 @@
+let rot13MapTable = {
+    "A": "N",
+    "B": "O",
+    "C": "P",
+    "D": "Q",
+    "E": "R",
+    "F": "S",
+    "G": "T",
+    "H": "U",
+    "I": "V",
+    "J": "W",
+    "K": "X",
+    "L": "Y",
+    "M": "Z",
+    "N": "A",
+    "O": "B",
+    "P": "C",
+    "Q": "D",
+    "R": "E",
+    "S": "F",
+    "T": "G",
+    "U": "H",
+    "V": "I",
+    "W": "J",
+    "X": "K",
+    "Y": "L",
+    "Z": "M",
+}
+
+function rot13(str) {
+    let tempArr = str.split("");
+    for (let i = 0; i < tempArr.length; ++i) {
+        if (tempArr[i] >= 'A' && tempArr[i] <= 'Z') {
+            tempArr[i] = rot13MapTable[tempArr[i]];
+        }
+    }
+    return tempArr.join("");
+}
+
+console.log(rot13("SERR PBQR PNZC"));
+console.log(rot13("SERR CVMMN!"));
+console.log(rot13("SERR YBIR?"));
+console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."));
