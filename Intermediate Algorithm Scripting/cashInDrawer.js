367b351fa211bd627ff1b91e2c15edace287daac
Gyandeep <gyandeepmehra370@gmail.com>
2022-02-02 09:12:22 +0530

    Add cash in drawer

---
+++ Intermediate Algorithm Scripting/cashInDrawer.js
@@ -0,0 +1,40 @@
+//return coins object of cid 
+
+function checkCashRegister(price, cash, cid) {
+    let conversionObj = {
+        "PENNY": 0.01,
+        "NICKEL": 0.05,
+        "DIME": 0.1,
+        "QUARTER": 0.25,
+        "ONE": 1,
+        "FIVE": 5,
+        "TEN": 10,
+        "TWENTY": 20,
+        "ONE HUNDRED": 100
+    };
+
+    let cidInCoins = {};
+    for (let c of cid) {
+        if (c[1] != 0) {
+            for (let curr in conversionObj) {
+                if (curr === c[0]) {
+                    let coinsOfCurrency = Math.ceil(c[1] / conversionObj[curr]);
+                    cidInCoins[c[0]] = coinsOfCurrency;
+                }
+            }
+        } else {
+            cidInCoins[c[0]] = 0;
+        }
+    }
+    return cidInCoins;
+}
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
