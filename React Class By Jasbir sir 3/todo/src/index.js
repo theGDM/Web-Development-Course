7194c4c02ddbb4dbb7f681da1bc68480c148aea8
Gyandeep <gyandeepmehra370@gmail.com>
2022-05-06 15:50:51 +0530

    Add index js file

---
+++ React Class By Jasbir sir 3/todo/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import PrintInstructorName from './PrintInstructorName';
+// import CounterClass1 from './CounterClass1';
+import CounterClass2 from './CounterClass2';
+// import CounterFunction from './CounterFunction';
+
+
+// ReactDOM.render(<PrintInstructorName />, document.getElementById("root"));
+// ReactDOM.render(<CounterClass1 />, document.getElementById("root"));
+ReactDOM.render(<CounterClass2 />, document.getElementById("root"));
+// ReactDOM.render(<CounterFunction />, document.getElementById("root"));
+
